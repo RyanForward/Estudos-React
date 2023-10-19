bec232cd82590e040ba8a1b5557c5e9664bd138e
Ryan Silva <ryanaugustoo71@hotmail.com>
2023-10-19 20:00:56 -0300

    Ajustando tudo

--- projetocards/src/pages/Admin/index.js
+++ projetocards/src/pages/Admin/index.js
@@ -1,23 +1,107 @@
-import { useState } from 'react'
-import { auth } from '../../firebaseConnection'
+import { useState, useEffect, cloneElement } from 'react'
+import { auth, db } from '../../firebaseConnection'
 import { signOut } from 'firebase/auth'
+import { addDoc, collection, onSnapshot, query, orderBy, where, doc, deleteDoc, updateDoc } from 'firebase/firestore'
 import './admin.css'
 
 export default function Admin(){
 
     const [tarefaInput, setTarefaInput] = useState('')
-    const [logoff, setLogoff] = useState(false);
+    const [user, setUser] = useState('');
+    const [tarefas, setTarefas] = useState([]);
+    const [edit, setEdit] = useState('');
 
-    function handleRegister(e){
+    useEffect(() => {
+        async function loadTarefas(){
+            const userDetail = localStorage.getItem("@detailuser");
+            setUser(JSON.parse(userDetail));
+
+            if (userDetail){
+                const data = JSON.parse(userDetail);
+
+                const tarefaRef = collection(db, "tarefas")
+                const q = query(tarefaRef, orderBy("created", "desc"), where("userUid", "==", data?.uid))
+                const unsub = onSnapshot(q, (snapshot) => {
+                    let lista = [];
+                    snapshot.forEach((doc) =>{
+                        lista.push({
+                            id: doc.id,
+                            tarefa: doc.data().tarefa,
+                            userUid: doc.data().userUid
+                        })
+                    })
+                    console.log(lista);
+                    setTarefas(lista);
+                })
+
+
+            }
+        }
+        loadTarefas()
+    }, [])
+
+    async function handleRegister(e){
         e.preventDefault();
-        alert("CLICOU");
+        if(tarefaInput == ''){
+            alert("Digite sua tarefa!");
+            return;
+        }
+
+        if(edit?.id){
+            handleUpdateTarefa();
+            return;
+        }
+
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user.uid
+        })
+        .then(() => {
+            console.log("Tarefa registrada!");
+            setTarefaInput('');
+        })
+        .catch((e) => {
+            console.log("Erro ao registrar" + e)
+        })
     }
+
 
     async function handleLogout(e){
         const confirm = window.confirm("Você está saindo da sua conta", ["Sim", "Não"]);
         if(confirm){
             await signOut(auth)
         }
+    }
+
+    async function deleteTarefa(id){
+        const docRef = doc(db, "tarefas", id);
+        await deleteDoc(docRef)
+    }
+
+    function editTarefa(item){
+        setTarefaInput(item.tarefa);
+        setEdit(item);
+    }
+
+    async function handleUpdateTarefa(){
+        console.log('1')
+        const docRef = doc(db, "tarefas", edit?.id);
+        console.log('2')
+        await updateDoc(docRef, {
+            tarefa: tarefaInput
+        })
+        .then(() => {
+            console.log('3')
+            console.log("Tarefa atualizada");
+            setEdit('');
+            setTarefaInput('');
+        })
+        .catch((e) => {
+            console.log("Erro ao atualizar: " + e);
+            setEdit('');
+            setTarefaInput('');
+        })
     }
 
     return(
@@ -27,24 +111,26 @@ export default function Admin(){
             <form onSubmit={handleRegister} className='form-admin'>
                 <textarea placeholder='Digite sua tarefa...' value={tarefaInput} onChange={(e) => setTarefaInput(e.target.value)}>
                 </textarea>
+                {Object.keys(edit).length > 0 ? (
+                <button className='btn-register' type='submit'> Atualizar tarefa</button>
+                ) : (
                 <button className='btn-register' type='submit'> Registrar tarefa</button>
+                )}
             </form>
 
-            <article className='task'>
-                <p>Estudar javascript e reactjs hoje a noite</p>
+            {tarefas.map((item) => (
+                <article key={item.id} className='task'>
+                <p>{item.tarefa}</p>
                 <div className='buttons'>
-                    <button className='btn-edit'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                    </svg>
+                    <button className='btn-edit' onClick={() => editTarefa(item)}>
+                        Editar
                     </button>
-                    <button className='btn-delete'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                    </svg>
+                    <button className='btn-delete' onClick={() => deleteTarefa(item.id)}>
+                        Concluir
                     </button>
                 </div>
             </article>
+            ))}
 
             <button className='btn-logout' onClick={handleLogout}>Sair</button>
 
